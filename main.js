88205953a694321bd02c9a95ba1722db568969d3
Tvisha-K <79145925+Tvisha-K@users.noreply.github.com>
2022-06-30 15:19:26 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,96 @@
+object = [];
+
+model_status = "";
+
+function setup(){
+
+    canvas = createCanvas(380,380);
+
+    canvas.center();
+
+    video = createCapture(VIDEO);
+
+    video.size(380,380);
+
+    video.hide();
+
+    cocossd = ml5.objectDetector("cocossd",modelLoaded);
+
+}
+
+function modelLoaded(){
+
+    document.getElementById("status").innerHTML = "Status : Detecting objects"
+
+    console.log("model has been loaded successfully");
+
+    model_status = true;
+
+    cocossd.detect(video, gotResults);
+
+}
+  
+
+function draw(){
+
+    image(video,0,0,380,380);
+
+    if (model_status != ""){
+
+        for(i=0; i < object.length; i++){
+
+            document.getElementById("status").innerHTML = "Status : Object Detected";
+            
+            document.getElementById("objects").innerHTML = "Number of objects detected : " + object.length;
+
+            object_name = object[i].label;
+
+            object_percentage = floor(object[i].confidence*100);
+
+            object_x = object[i].x;
+
+            object_y = object[i].y;
+
+            object_width = object[i].width;
+
+            object_height = object[i].height;
+
+            r = floor(random(256));
+
+            g = floor(random(256));
+
+            b = floor(random(256));
+
+            fill(r,g,b);
+
+            text(object_name + " " + object_percentage + "%" , object_x , object_y);
+
+            stroke(r,g,b);
+
+            noFill();
+
+            rect(object_x , object_y , object_width , object_height);
+
+        }
+
+    }
+   
+}
+
+function gotResults(e,r){
+
+    if (e){
+
+       console.error(e) 
+
+    }
+
+    else{
+
+        console.log(r);
+
+        object = r;
+
+    }
+
+}
